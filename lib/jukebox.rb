36474c1e1a3783cff73b51ac1a0ea5f02582e1e6
Joey Luis Echevarria <jechevarria719@gmail.com>
2019-08-07 21:27:11 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -5,4 +5,10 @@
    "tokyo police club - wait up",
    "sufjan stevens - too much",
    "the naked and the famous - young blood",
+   "(far from) home - tiga",
+   "the cults - abducted",
+   "phoenix - consolation prizes",
+   "harry chapman - cats in the cradle",
+   "amos lee - keep it loose, keep it tight"
    ]
+ end
