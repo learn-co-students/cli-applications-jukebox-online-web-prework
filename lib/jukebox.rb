82efeb3ad62e79d2cf0301194e9c6d62eedf13f4
Amanda Rolon <abrolon87@gmail.com>
2019-11-30 17:56:50 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,15 +1,15 @@
-# Add your code here
-songs = [
-  "Phoenix - 1901",
-  "Tokyo Police Club - Wait Up",
-  "Sufjan Stevens - Too Much",
-  "The Naked and the Famous - Young Blood",
-  "(Far From) Home - Tiga",
-  "The Cults - Abducted",
-  "Phoenix - Consolation Prizes",
-  "Harry Chapin - Cats in the Cradle",
-  "Amos Lee - Keep It Loose, Keep It Tight"
-]
+require "pry"# Add your code here
+#songs = [
+#  "Phoenix - 1901",
+# #  "Tokyo Police Club - Wait Up",
+#   "Sufjan Stevens - Too Much",
+#   "The Naked and the Famous - Young Blood",
+#   "(Far From) Home - Tiga",
+#   "The Cults - Abducted",
+#   "Phoenix - Consolation Prizes",
+#   "Harry Chapin - Cats in the Cradle",
+#   "Amos Lee - Keep It Loose, Keep It Tight"
+# ]
 
 #def say_hello(name)
 #  "Hi #{name}!"
@@ -28,11 +28,7 @@ def help
   puts "- exit : exits this program"
 end
 
-def list(songs)
-  songs.each_with_index do |song, index|
-    puts "#{index+1}. #{song}"
-  end
-end
+
 
 def play(songs)
   puts "Please enter a song name or number:"
@@ -52,13 +48,31 @@ def play(songs)
   end
 end
 
+def list(songs)
+  songs.each_with_index do |x, y|
+    puts "#{y+1}. #{x}"
+  end
+end
+
+
 def exit_jukebox
   puts "Goodbye"
 end
 
 
 def run(songs)
-
-
-
+  while true
+  puts "Please enter a command:"
+  command = gets.strip
+    if command == "help"
+      help
+    elsif command == "list"
+      list(songs)
+    elsif command == "play"
+      play(songs)
+    elsif command == "exit"
+      exit_jukebox
+      break
+    end
+  end
 end
